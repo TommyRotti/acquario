f3fde949addea2dad60755b15b0e67ec601d065e
TOMMASO.CORSINI <TOMMASO.CORSINI@buonarroti.tn.it>
2023-10-09 10:22:10 +0200

    feat: method constructor Pesce

--- src/Pesce.java
+++ src/Pesce.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class Pesce {
+public class Pesce {
+    private boolean vita;
+    private String colore, specie;
+
+    public Pesce(boolean vita, String colore, String specie){
+        if(vita==true || vita==false){
+            this.vita = vita;
+        }
+        if(!(colore.equals(""))){
+            this.colore = colore;
+        }
+        if(!(specie.equals(""))){
+            this.specie = specie;
+        }
+    }
+    
 }
