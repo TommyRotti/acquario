3270918f7c83f33a6d29cc3f8530391dc980b126
TommyRotti <tommaso.corsini@buonarroti.tn.it>
2023-10-11 15:54:55 +0200

    feat methods Constructor Acquario

--- src/Acquario.java
+++ src/Acquario.java
@@ -1,4 +1,27 @@
 public class Acquario {
 
-    Pesce[] pesci;
+    private Pesce[] pesci;
+    private int[] dimensioni;
+
+    public Acquario(int[] dim, int dimArray){
+        this.pesci = new Pesce[dimArray];
+        for(int i=0; i<dim.length; i++){
+            if(dim[i]>0){
+              this.dimensioni[i] = dim[i];
+            }
+        }
+    }
+
+    public Acquario(int[] dim, Pesce[] pesci){
+        this.pesci = new Pesce[pesci.length];
+        for(int i=0; i<dim.length; i++){
+            if(dim[i]>0){
+                this.dimensioni[i] = dim[i];
+            }
+        }
+        for(int i=0; i<this.pesci.length; i++){
+            this.pesci[i] = pesci[i].clone();
+        }
+    }
+
 }
